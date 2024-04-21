274a595eb3007e1dbcd86e39695753f22d4741f7
brunobenedicto <brunobenedicto.rocha@gmail.com>
2024-04-21 00:45:12 -0300

    mais um bocadinho

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 function playIntro() {
   var introSound = document.getElementById('soundIntro');
-  introSound.play();
+  if (introSound) {
+    introSound.play().catch(function(error) {
+      // Trate qualquer erro que ocorra ao tentar reproduzir o som
+      console.error('Erro ao reproduzir o som intro:', error);
+    });
+  } else {
+    console.error('Elemento de áudio "soundIntro" não encontrado');
+  }
 }
-
 
 // Função para abrir o modal
 function openModal(totalPrice, products) {
@@ -11,73 +17,25 @@ function openModal(totalPrice, products) {
   productList.innerHTML = "";
   products.forEach(function(product) {
       var li = document.createElement("li");
-      li.innerText = product.name + " - Quantidade: " + product.quantity;
+      li.innerText = product.quantity + " - " + product.name;
       productList.appendChild(li);
   });
   document.querySelector('.modal').style.display = 'block';
 }
 
+// Função para fechar o modal
+function closeModal() {
+  document.querySelector('.modal').style.display = 'none';
+}
 
-  // Função para fechar o modal
-  function closeModal() {
-    document.querySelector('.modal').style.display = 'none';
-  }
-
-  // Botão de compra
-  document.querySelector('.buy-button').addEventListener('click', function() {
-    var totalPrice = 0;
-    var products = [];
-    var cards = document.querySelectorAll('.card');
-    cards.forEach(function(card) {
-        console.log(card); // Verifica o conteúdo de card
-        var productPriceElement = card.querySelector('.price');
-        console.log(productPriceElement); // Verifica se o elemento com a classe 'price' foi encontrado
-        var productName = card.querySelector('p').innerText;
-        var productPrice = parseFloat(productPriceElement.innerText.replace('R$ ', ''));
-        var quantity = parseInt(card.querySelector('.quantity-input').value);
-        totalPrice += productPrice * quantity;
-        if (quantity > 0) {
-            products.push({ name: productName, quantity: quantity });
-        }
-    });
-    openModal(totalPrice, products);
+document.getElementById("pay-button").addEventListener("click", function() {
+  var clickSoundPay = document.getElementById('paySound');
+  clickSoundPay.play()
+  // Aqui você pode adicionar a lógica para processar o pagamento
+  // Por exemplo, você pode redirecionar o usuário para uma página de pagamento ou exibir uma mensagem de confirmação
+  alert("Pagamento processado com sucesso!");
+  closeModal();
 });
-
-  // Botões de quantidade
-  var clickSound1 = document.getElementById('clickButton+');
-  var clickSound2 = document.getElementById('clickButton-');
-  var quantityButtons = document.querySelectorAll('.quantity-button');
-  quantityButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      var input = button.parentElement.querySelector('.quantity-input');
-      var value = parseFloat(input.value);
-      if (button.innerText === '-' && value > 0) {
-        input.value = value - 1;
-        console.log(input.value)
-        clickSound2.play();
-      } else if (button.innerText === '+') {
-        input.value = value + 1;
-        clickSound1.play();
-        console.log(input.value)
-        console.log(typeof(input.value))
-      }
-    });
-  });
-
-  // Fechar o modal ao clicar fora da área do modal
-  window.onclick = function(event) {
-    var modal = document.querySelector('.modal');
-    if (event.target == modal) {
-      closeModal();
-    }
-  };
-
-  document.getElementById("pay-button").addEventListener("click", function() {
-    // Aqui você pode adicionar a lógica para processar o pagamento
-    // Por exemplo, você pode redirecionar o usuário para uma página de pagamento ou exibir uma mensagem de confirmação
-    alert("Pagamento processado com sucesso!");
-});
-
 
 function addQuantityButtonListeners() {
   var clickSound1 = document.getElementById('clickButton+');
@@ -107,36 +65,63 @@ function loadProducts() {
   .then(data => {
       data.forEach(product => {
           var card = document.createElement('div');
-          card.classList.add('card', 'col-9','col-md-4', 'm-3'); // Adicionando a classe 'col-4'
+          card.classList.add('card', 'col-9','col-md-3','col-lg-2', 'm-2'); // Adicionando a classe 'col-4'
           card.innerHTML = `
               <div class="col-sm">
                   <img src="${product.image}" alt="${product.name}">
               </div>
               <div class="col-sm">
-                  <p>${product.name}</p>
+                  <p class="titulo-card">${product.name}</p>
               </div>
-              <div class="col-sm">
-                  <p>R$ <span class="price">${product.price.toFixed(2)}</span></p>
+              <div class="preco-card col-sm">
+                  <p>R$ <span>${product.price.toFixed(2)}</span></p>
               </div>
-              <div class="contador col-sm container ">
-                  <div class="input-group container">
-                      <div class="input-group-prepend container">
-                          <button class="quantity-button btn btn-outline-secondary" type="button">-</button>
-                          <input type="number" class="quantity-input form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" value="0" readonly>
-                          <button class="quantity-button btn btn-outline-secondary" type="button">+</button>
-                      </div>
-                  </div>
+              
+              <div class="contador input-group col-sm">
+                <div class="input-group-prepend">
+                  <button class="quantity-button btn" type="button">-</button>
+                  <input type="number" class="quantidade-card quantity-input form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" value="0" readonly>
+                  <button class="quantity-button btn" type="button">+</button>
+                </div>
               </div>
           `;
           document.getElementById('product-container').appendChild(card); // Modificado para selecionar o container correto
       });
       // Adiciona event listeners aos botões de quantidade após carregar os produtos
       addQuantityButtonListeners();
+      
+      // Adiciona o evento de clique ao botão de compra
+      document.querySelector('.buy-button').addEventListener('click', function() {
+        playIntro();
+        var totalPrice = 0;
+        var products = [];
+        var cards = document.querySelectorAll('.card');
+        cards.forEach(function(card) {
+            console.log(card); // Verifica o conteúdo de card
+            var productPriceElement = card.querySelector('.preco-card span');
+            console.log(productPriceElement); // Verifica se o elemento com a classe 'price' foi encontrado
+            var productName = card.querySelector('.titulo-card').innerText;
+            var productPrice = parseFloat(productPriceElement.innerText);
+            var quantity = parseInt(card.querySelector('.quantity-input').value);
+            totalPrice += productPrice * quantity;
+            if (quantity > 0) {
+                products.push({ name: productName, quantity: quantity });
+            }
+        });
+        openModal(totalPrice, products);
+      });
   })
   .catch(error => console.error('Erro ao carregar os produtos:', error));
 }
 
-
-
 // Chame a função para carregar os produtos ao carregar a página
-window.onload = loadProducts;
+window.onload =function(){
+  loadProducts();
+} 
+  
+window.onclick = function(event) {
+  var modal = document.querySelector('.modal');
+  if (event.target == modal) {
+    closeModal();
+  }
+};
